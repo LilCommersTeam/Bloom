a47b97ae84653a8bf84efa37e2bec95e53b14fb0
LC Team <lilcoomers.esi@gmail.com>
2024-04-18 19:05:21 +0330

    create sign up folders 18 April

--- src/app/Components/Sign-up/sign-up.module.ts
+++ src/app/Components/Sign-up/sign-up.module.ts
@@ -1,9 +1,9 @@
 import {RouterOutlet} from "@angular/router";
 import {CommonModule} from "@angular/common";
-import { SignUpFirstStepComponent} from "./sign-up-first-step.component";
+import { SignUpFirstStepComponent} from "./First-step/sign-up-first-step.component";
 import {NgModule} from "@angular/core";
-import {SignUpSecondStepComponent} from "./sign-up-second-step.component";
-import {SignUpThirdStepComponent} from "./sign-up-third-step.component";
+import {SignUpSecondStepComponent} from "./Second-step/sign-up-second-step.component";
+import {SignUpThirdStepComponent} from "./Third-step/sign-up-third-step.component";
 
 @NgModule({
   imports: [
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import {SignUpFirstStepComponent} from "./Components/Sign-up/sign-up-first-step.component";
-import {SignUpSecondStepComponent} from "./Components/Sign-up/sign-up-second-step.component";
-import {SignUpThirdStepComponent} from "./Components/Sign-up/sign-up-third-step.component";
+import {SignUpFirstStepComponent} from "./Components/Sign-up/First-step/sign-up-first-step.component";
+import {SignUpSecondStepComponent} from "./Components/Sign-up/Second-step/sign-up-second-step.component";
+import {SignUpThirdStepComponent} from "./Components/Sign-up/Third-step/sign-up-third-step.component";
 
 export const routes: Routes = [
   { path: 'Sign-up-first-step', component: SignUpFirstStepComponent },

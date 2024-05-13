40ec793936871793db1febfda3098b8b595a1050
LC Team <lilcoomers.esi@gmail.com>
2024-05-13 20:31:55 +0330

    Bugfix chat component  13 May

--- src/app/Components/Chat/chat.component.ts
+++ src/app/Components/Chat/chat.component.ts
@@ -1,31 +1,36 @@
-import { AfterContentInit, AfterViewInit, Component, ElementRef} from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {Component, ElementRef, EventEmitter, Output} from '@angular/core';
 import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'chat-component',
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css',
-  
+
 })
 
 export class ChatComponent{
-  
+
   @ViewChild('messege') input!: ElementRef;
+  @Output() newItemEvent = new EventEmitter<string>();
+  messegeControl: boolean = true;
 
-  console.log(this.input.value);
-  
 
-  messegeControl: boolean = false;
+  get(value:any) {
+    this.newItemEvent.emit(value);
+    this.input.nativeElement.value = '';
+    }
+
+
+
 
  /*  sendMessege(){
     const messege = this.messegeControl.value;
-    
+
     if(messege){
       this.messegeControl.setValue('');
     }
   }*/
 
 
-  
+
 }
--- src/app/Components/Dashboard/dashboard.component.ts
+++ src/app/Components/Dashboard/dashboard.component.ts
@@ -9,11 +9,17 @@ import { Component} from '@angular/core';
 export class DashboardComponent {
 
 ChatIsClosed : boolean = true;
+items : String[] = []
+notification : String = 'hi, how can i help you?'
+warningForLogin : String = 'please log in before sending your messege'
+
 
 toggleChat(){
   this.ChatIsClosed = !this.ChatIsClosed;
 }
+addItem(newItem: string) {
+    this.items.push(newItem);
+  }
 
 
-  
 }
